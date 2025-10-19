c36b6fed2d61f3814ff3dee3f5c4f2a7cf7ab02f
Steven Gilbert Ayala <stevengilbertayala@gmail.com>
2025-10-19 15:47:29 -0300

    Revert "fix health check"
    This reverts commit 94fd41cad8585de074b567bd5b6761c6ee4d711a.

--- test_local.py
+++
@@ -1,9 +0,0 @@
-"""
-Script para probar la aplicación localmente
-"""
-
-import uvicorn
-
-if __name__ == "__main__":
-    print("Testing app.py locally...")
-    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload=True)
